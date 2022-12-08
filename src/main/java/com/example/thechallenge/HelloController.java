613347a22fb6faa9f7aa9e37696f4dfab5ffa8d0
aluqu <22153047@student.hhs.nl>
2022-12-08 13:18:30 +0100

    TEST cpmmiy

--- src/main/java/com/example/thechallenge/HelloController.java
+++ src/main/java/com/example/thechallenge/HelloController.java
@@ -3,7 +3,7 @@ package com.example.thechallenge;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class GUI {
     @FXML
     private Label welcomeText;
 
