e6e357d24eb584181b9e6b3d3e50ba0cfd4e3c2c
noran <22142088@student.hhs.nl>
2022-12-08 13:35:45 +0100

    TESt45745674567

--- src/main/java/com/example/thechallenge/GUI.java
+++ src/main/java/com/example/thechallenge/GUI.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class GUI {
     public static void main(String[] args){
-        System.out.println("Hello GUI");
+        System.out.println("Hellooo GUI");
 
     }
 }
