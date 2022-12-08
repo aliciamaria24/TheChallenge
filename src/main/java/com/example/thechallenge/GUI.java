03a0ecbb670751f23d22e89aee3a77d0ff086395
aluqu <22153047@student.hhs.nl>
2022-12-08 15:40:25 +0100

    JAVAFX FXML file

--- src/main/java/com/example/thechallenge/GUI.java
+++ src/main/java/com/example/thechallenge/GUI.java
@@ -1,24 +1,36 @@
 package com.example.thechallenge;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import javax.swing.*;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 
 
-public class GUI {
+public class GUI implements ActionListener, AncestorListener {
+
+    int count = 0;
+
     public GUI() {
 
         JFrame frame = new JFrame ();
 
-        JButton button = new JButton("Click me!")
+        JButton button = new JButton("Click me!");
+        button.addAncestorListener(this);
+
+        JLabel label = new JLabel("Number of clicks: 0");
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 10, 30, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,6 +42,26 @@ public class GUI {
     public static void main(String[] args){
        new GUI();
 
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void ancestorAdded(AncestorEvent event) {
+
+    }
+
+    @Override
+    public void ancestorRemoved(AncestorEvent event) {
+
+    }
+
+    @Override
+    public void ancestorMoved(AncestorEvent event) {
 
     }
 }
