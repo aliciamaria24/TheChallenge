54707c98e3d85e9a94b8daf785d4f5df1759c716
noran <22142088@student.hhs.nl>
2022-12-08 13:12:02 +0100

    Commit GUI

--- src/main/java/GUI.java
+++
@@ -1,8 +0,0 @@
-
-
-public class GUI {
-    public static void main(String[] args){
-
-        JFrame frame = new JFrame();
-    }
-}
---
+++ src/main/java/com/example/thechallenge/GUI.java
@@ -0,0 +1,12 @@
+package com.example.thechallenge;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+
+public class GUI {
+    public static void main(String[] args){
+
+    }
+}
