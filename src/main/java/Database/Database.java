d01582fb15afcd167028215479a55b2978c176ae
AmitRosaria11 <13111795@student.hhs.nl>
2022-12-22 15:36:09 +0100

    Nieuwe method aangemaakt in Database.java

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -14,38 +14,52 @@ public class Database {
         createConnection();
 
     }
+
     public static void createConnection() {
-     try {
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "Amit23@");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT luchtkwaliteit_binnen_CO2 FROM luchtkwaliteit_binnen");
-        while (rs.next()) {
-            int gemiddelde = rs.getInt("luchtkwaliteit_binnen_CO2");
-            System.out.println(gemiddelde);
-        }
-        System.out.println("Database Connection Succes");
-    } catch (
-    SQLException ex) {
-        Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-    }
-}
-
-
-    private Connection connect() {
-        Connection conn = null;
-        String driver = "com.mysql.cj.jdbc.Driver";
-        // MySQL connection string, pas zonodig het pad aan:
-        String connection = "jdbc:mysql://localhost:3306/project";
-        String user = "root";
-        String password = "Amit23@";
         try {
-            Class.forName(driver);
-            conn = DriverManager.getConnection(connection, user, password);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "Amit23@");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT luchtkwaliteit_binnen_CO2 FROM luchtkwaliteit_binnen ORDER BY luchtkwaliteit_binnen_id DESC LIMIT 1 ");
+            while (rs.next()) {
+                int gemiddelde = rs.getInt("luchtkwaliteit_binnen_CO2");
+                System.out.println(gemiddelde);
+
+            }
+            System.out.println("Database Connection Succes");
+        } catch (
+                SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return conn;
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "Amit23@");
+            Statement stmt = con.createStatement();
+            ResultSet luchtbuiten = stmt.executeQuery("SELECT luchtkwaliteit_buiten_fijnstof FROM luchtkwaliteit_buiten ORDER BY luchtkwaliteit_buiten_id DESC LIMIT 1 ");
+            while (luchtbuiten.next()) {
+                int gemiddelde = luchtbuiten.getInt("luchtkwaliteit_buiten_fijnstof");
+                System.out.println(gemiddelde);
+            }
+        } catch (
+                SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
     }
+
+        private Connection connect () {
+            Connection conn = null;
+            String driver = "com.mysql.cj.jdbc.Driver";
+            // MySQL connection string, pas zonodig het pad aan:
+            String connection = "jdbc:mysql://localhost:3306/project";
+            String user = "root";
+            String password = "Amit23@";
+            try {
+                Class.forName(driver);
+                conn = DriverManager.getConnection(connection, user, password);
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+            return conn;
+        }
 
 }
 
