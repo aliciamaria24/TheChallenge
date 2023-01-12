4756005b953f551082abeda7829017739b6a64c5
noran <22142088@student.hhs.nl>
2023-01-12 10:31:13 +0100

    Controller java file aangemaakt en fxml afgemaakt

--- src/main/java/FXML/GUILogin.java
+++ src/main/java/FXML/GUILogin.java
@@ -1,6 +1,26 @@
 package FXML;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class GUILogin  {
+import javax.swing.tree.ExpandVetoException;
+
+public class GUILogin extends Application {
+
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("GUILogin.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.show();
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
 }
--- src/main/java/FXML/LoginController.java
+++ src/main/java/FXML/LoginController.java
@@ -1,21 +1,43 @@
-//package FXML;
-//
-//import javafx.fxml.Initializable;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.event.ActionEvent;
-//
-//public class LoginController implements Initializable {
-//
-//    private Label label;
-//
-//    private TextField txtname;
-//
-//    private Button btn;
-//
-//    private GUILogin(ActionEvent event){
-//
-//        JOptionPane.showMessageDialog(null, "Hi");
-//    }
-//}
+package FXML;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoginController implements Initializable {
+
+    private Label label;
+
+    private TextField txtname;
+
+    private Button btn;
+
+    private PasswordField txtpassword;
+
+    void login(ActionEvent event){
+
+        String username = txtname.getText();
+        String password = txtpassword.getText();
+
+        if(username.equals("") && password.equals("")){
+
+            JOptionPane.showMessageDialog(null, "Hi");
+        }
+        else{
+            try {
+                Class.forName("ccm.mysql.jdbc.Driver");
+
+
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+
+    }
+}
